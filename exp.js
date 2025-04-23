48ee0b37a5b64a6121b84d3656095723dce568aa
mitp1220 <mit214p@gmail.com>
2025-04-23 13:34:59 +0530

    Add files via upload

--- exp.js
+++ exp.js
@@ -1,18 +1,18 @@
-const expr = require('express')
+const expr = require("express")
 const app = expr();
 
-// url:localhost:3021/user/342/test/889
-// app.get("/user/:userid/test/:test1",(req,res)=>{
-//     res.send(req.params);
-// });
+// app.get("/",(req,res)=> {
+//     res.set("Content-Type","text/plain")
+//     res.write("<b>hello world</b>");
+//     res.send();
+// }); 
+// app.get("/about",(req,res)=> {
+//     res.set("Content-Type","text/html")
+//     res.write("<h1>hello world</h1>");
+//     res.send();
+// }); 
 
-// url:localhost:3021/things/practice/12345
-// app.get("/things/:name/:id",(req,res)=>{      
-//     res.send(req.params);
-// });
-
-// url:localhost:3021/flights/ADI-NYC
-app.get("/flights/:from-:to",(req,res)=>{
-    res.send(req.params);
+app.get("/:id",(req,res) => {
+    res.send("the id you specified is"+req.params.id);
 });
-app.listen(3021)
+app.listen(2225);
