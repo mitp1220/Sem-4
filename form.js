171ac77bd80f941e8291128e5232dfeef12f1eaf
mitp1220 <mit214p@gmail.com>
2025-07-15 10:04:28 +0530

    Add files via upload

--- form.js
+++ form.js
@@ -1,11 +1,28 @@
-const expr = require('express')
-const app = expr();
+const mg = require("mongoose")
+const expr = require("express")
+var app = expr()
 
-app.use(expr.static("./",{index:"form.html"}))
-app.get('/process_get',(req,res)=>{
-    response = {Username:req.query.FullName,Password:req.query.Password};
-    console.log(response)
-    res.send(response)
+mg.connect("mongodb://127.0.0.1:27017/mit258").then(()=> {
+    console.log("connection successful")
+}).catch((err)=> {
+    console.error(err)
+});
+mg.pluralize(null)
+
+const myschema = new mg.Schema({
+    username: {type:String,required:true},
+    password:{type:String,required:true},
 });
 
-app.listen(2548)
+const data = new mg.model("formdata",myschema)
+
+app.use(expr.static(__dirname,{index:"form.html"}))
+
+app.get("/redirect",(req,res)=>{
+    const persondata = new data({username:req.query.uname,
+                                password:req.query.pwd})
+persondata.save()
+res.send()
+})
+
+app.listen(5600)
